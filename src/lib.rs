160ca178134abbe0917225227be32b6383869465
Joey <jyudel@gmail.com>
2022-10-02 20:49:48 -0400

    Better comment

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,18 @@ use indicatif::{ProgressIterator, ProgressStyle, ProgressBarIter};
 
 const PBAR_TEMPLATE: &str = "{msg} |{wide_bar}| {pos}/{len} [{elapsed_precise}>{eta_precise}]";
 
-/// A very lazy wrapper for any ProgressIterator with know length
-/// Looks nice enough as a default
-/// Style heavily borrowing from [tqdm](https://github.com/tqdm/tqdm)
+/// A very lazy wrapper for any ProgressIterator with known length.
+/// 
+/// Looks nice enough as a default.
+/// 
+/// Style heavily borrowing from [tqdm](https://github.com/tqdm/tqdm).
+/// 
 /// Example:
-/// ''' 
-/// # let mut x = 0; 
+/// ```
 /// for i in pbar(0..1_000_000, 1_000_000) {
-/// #    x = i;
+/// // Do whatever
 /// }
-/// '''
+/// ```
 pub fn pbar<I: ProgressIterator>(it: I, len: usize) -> ProgressBarIter<I>{
     let pbar = indicatif::ProgressBar::new(len as u64)
             .with_style(
